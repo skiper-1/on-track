e95bed5987ec6894f42f09761d847d492c84dd1d
Kirill Stetsenko <stetsenkoki@gmail.com>
2025-03-18 14:24:25 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="h-screen bg-gray-800">
+    <div class="text-center text-red-500">On Track</div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
